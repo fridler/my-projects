1b7f9381e1b0214f5323c6c7235bd3640fd11c27
fridler <fridler@gmail.com>
2021-04-26 18:48:39 +0300

    fixing navbar to support image click

--- src/components/NavbarAppComponent/NavbarAppComponent.js
+++ src/components/NavbarAppComponent/NavbarAppComponent.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 import './NavbarAppComponent.css'
+
 export default function NavbarAppComponent() {
+
+    const currentPath = usePathName();
+
     return (
         <Container className="c-navbar">
             <Navbar bg="white" variant="light">
                 <Navbar.Brand className="c-navbar-brand">
-                    <img src="https://cdn2.iconfinder.com/data/icons/bold-outline-v1/512/camera_cine_cinema_film-512.png" alt=""></img>
-                    Movie App
+                    <a href="#home">
+                        <img src="https://cdn2.iconfinder.com/data/icons/bold-outline-v1/512/camera_cine_cinema_film-512.png" alt="" />
+                    </a>
                 </Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#actors">Actors</Nav.Link>
-                    <Nav.Link href="#movies">Movies</Nav.Link>
+                    <Nav.Link href="#home" active={currentPath === "/" || currentPath === "/home"}>Home</Nav.Link>
+                    <Nav.Link href="#actors" active={currentPath === "/actors"}>Actors</Nav.Link>
+                    <Nav.Link href="#movies" active={currentPath.includes("movies")}>Movies</Nav.Link>
                 </Nav>
             </Navbar>
         </Container>
