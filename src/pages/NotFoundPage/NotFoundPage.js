830f0a0851fea626d3df4011200095b199e571e9
fridler <fridler@gmail.com>
2021-04-26 18:21:06 +0300

    adding not found page

---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import './NotFoundPage.css'
+
+export default function NotFoundPage() {
+    return (
+        <Container className="p-not-found">
+            <img src="https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png"></img>
+        </Container>
+    )
+}
