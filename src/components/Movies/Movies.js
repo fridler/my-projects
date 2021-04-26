5e17ece37bd495f6a410031b537401c38af85ebc
fridler <fridler@gmail.com>
2021-04-26 18:46:51 +0300

    add movie through search engine

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovieCard from '../../components/MovieCard/MovieCard';
+
+export default function Movies({ movies }) {
+    let moviesCards;
+    if (movies) {
+        moviesCards = movies.map((movie, index) => <MovieCard key={index} movie={movie}></MovieCard>);
+    }
+    return (
+        <div>
+            {moviesCards}
+        </div>
+    )
+}
--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -2,13 +2,16 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Container, Spinner } from 'react-bootstrap';
 import { useParams } from 'react-router';
-import MovieCard from '../../components/MovieCard/MovieCard';
+import Movies from '../../components/Movies/Movies';
+import SearchBox from '../../components/SearchBox/SearchBox';
 import MovieModel from '../../model/MovieModel';
 import './MoviesPage.css';
 
 function MoviesPage() {
     const { actorId } = useParams();
     const [movies, setMovies] = useState([]);
+    const [results, setResults] = useState([]);
+    const [searchText, setSearchText] = useState("")
 
     let movieList = [];
     //if it comes from actors page by selecting an actor then the list will be the specific actor movies
@@ -16,35 +19,74 @@ function MoviesPage() {
 
     useEffect(() => {
 
-        const searchURL = actorId ? `https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=d5fc0ebcdc97957658216ba08b5e9436` :
-            "https://api.themoviedb.org/3/movie/top_rated?api_key=d5fc0ebcdc97957658216ba08b5e9436";
-
-        axios.get(searchURL).then(res =>
-            actorId ? res.data.cast.slice(0, 19).map(plainMovie => addMovie(plainMovie)) :
-                res.data.results.slice(0, 19).map(plainMovie => addMovie(plainMovie))
-        );
+        if (actorId) {
+            const searchURL = `https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=d5fc0ebcdc97957658216ba08b5e9436`;
+            axios.get(searchURL).then(res => {
+                res.data.cast.slice(0, 20).map(plainMovie => {
+                    const movie = new MovieModel(plainMovie.id, plainMovie.title, "https://image.tmdb.org/t/p/w500" + plainMovie.poster_path, plainMovie.overview);
+                    //get info regarding each movie
+                    additionalInfo(movie, actorId);
+                });
+            });
+        }
     }, [actorId]);
 
 
-    function addMovie(plainMovie) {
-        const movie = new MovieModel(plainMovie.id, plainMovie.title, "https://image.tmdb.org/t/p/w500" + plainMovie.poster_path, plainMovie.overview);
-        //get info regarding each movie
+    function additionalInfo(movie, actorId) {
         const promise1 = axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=d5fc0ebcdc97957658216ba08b5e9436`);
         const promise2 = axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=d5fc0ebcdc97957658216ba08b5e9436`);
         Promise.all([promise1, promise2]).then(responses => {
             movie.setStars(responses[0].data.cast.slice(0, 9).map(x => x.name).join(", "));
             movie.setDirector(responses[0].data.crew.find(x => x.job === 'Director').name);
             movie.setRuntime(responses[1].data.runtime);
-            movieList = movieList.concat(movie);
+            if (actorId) {
+                movieList = movieList.concat(movie);
+            } else {
+                movieList = movies.concat(movie);
+            }
             setMovies(movieList);
         });
 
     }
+    function handleSearchChange(newSearchText) {
+        setSearchText(newSearchText);
+        if (newSearchText) {
+            //call to TMDB
+            const searchURL = "https://api.themoviedb.org/3/search/movie?api_key=d5fc0ebcdc97957658216ba08b5e9436&query=" + newSearchText;
+            axios.get(searchURL).then(response => {
+                setResults(response.data.results);
+            });
+        } else {
+            setResults([]);
+        }
+    }
+
+    function addMovie(resultIndex) {
+        if (movies && !movies.some(el => parseInt(el.id) === parseInt(results[resultIndex].id))) {
+            const movie = new MovieModel(results[resultIndex].id,
+                results[resultIndex].title,
+                "https://image.tmdb.org/t/p/w500" + results[resultIndex].poster_path,
+                results[resultIndex].overview);
+            additionalInfo(movie, null);
+        }
+        setResults([]);
+        setSearchText("");
+    }
 
     return (
         <Container>
-            <h1 className="text-center">Top Rated Films</h1>
-            {movies ? movies.map((movie, index) => <MovieCard key={index} movie={movie}></MovieCard>) : <Spinner />}
+            <h1 className="p-title">Movies</h1>
+            <SearchBox
+                placeHolder="Search Movie..."
+                searchText={searchText}
+                onSearchChange={handleSearchChange}
+                results={results.map(result => result.title)}
+                onResultSelected={addMovie} />
+            <div className="p-movie-results">
+                {movies.length > 0 ?
+                    <Movies movies={movies} /> :
+                    <Spinner className="p-movie-spinner" />}
+            </div>
         </Container>
     )
 }
