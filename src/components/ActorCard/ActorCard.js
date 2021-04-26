6bcb62de79a79b7f6f74bb6bc872e0a916391ebf
fridler <fridler@gmail.com>
2021-04-26 17:27:21 +0300

    change display for card body

---
+++ src/components/ActorCard/ActorCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import './ActorCard.css';
+export default function ActorCard({ actor }) {
+    return (
+        <Card style={{ width: '18rem' }} className="col-sm-6 col-md-3 c-actor-card">
+            <Card.Img variant="top" src={actor.img} />
+            <Card.Body className="actor-c-body">
+                <Card.Title>
+                    <a href={actor.imdb} target="_blank">
+                        {`${actor.fname} ${actor.lname}`}
+                    </a>
+                </Card.Title>
+                <Card.Text>
+                    {actor.age}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
