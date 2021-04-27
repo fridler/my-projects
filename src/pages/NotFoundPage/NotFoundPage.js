08e8b943413477a41794ea90873acf63a379ccbd
fridler <fridler@gmail.com>
2021-04-27 23:11:28 +0300

    Updating Style

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -4,7 +4,7 @@ import './NotFoundPage.css'
 export default function NotFoundPage() {
     return (
         <Container className="p-not-found">
-            <img src="https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png" alt=""></img>
+            <img src="https://cdn3.iconfinder.com/data/icons/network-and-communications-10/32/network_Error_lost_no_page_not_found-256.png" alt=""></img>
         </Container>
     )
 }
